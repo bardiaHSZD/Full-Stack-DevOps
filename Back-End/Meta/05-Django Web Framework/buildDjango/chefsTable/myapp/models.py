e075bf10b133483c26220218339e8f1dc620af86
bardia@ualberta.ca <bardia@ualberta.ca>
2024-08-01 23:40:57 -0700

    Upload Models Stage I

--- Back-End/Meta/05-Django Web Framework/buildDjango/chefsTable/myapp/models.py
+++ Back-End/Meta/05-Django Web Framework/buildDjango/chefsTable/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from unicodedata import name
+
+class Menu(models.Model):
+    name = models.CharField(max_length= 100)
+    cuisine = models.CharField(max_length= 100)
+    price = models.IntegerField()
+    
+    def __str__(self):
+        return self.name + ":" +  self.cuisine
