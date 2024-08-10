0dbf0c2cba5ae97cacba9648b3635ad578fac237
Bardia Hassanzadeh <bardia.hszd@gmail.com>
2024-08-10 07:27:09 -0700

    Upload Admin Permissions

--- Back-End/Meta/05-Django Web Framework/buildDjango/chefsTable/myapp/models.py
+++ Back-End/Meta/05-Django Web Framework/buildDjango/chefsTable/myapp/models.py
@@ -16,4 +16,19 @@ class Logger(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     time_log = models.TimeField()
+
+
+class Reservations(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    contact = models.CharField('Phone number', max_length=300)
+    time = models.TimeField()
+    count = models.IntegerField()
+    notes = models.CharField(max_length=300, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
     
