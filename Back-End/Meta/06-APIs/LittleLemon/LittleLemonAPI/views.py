f88ea1beb5f04a520986a1d3366c40a7ab9582c0
Bardia Hassanzadeh <bardia.hszd@gmail.com>
2024-08-17 04:03:49 -0700

    Upload Serializer Codes

--- Back-End/Meta/06-APIs/LittleLemon/LittleLemonAPI/views.py
+++ Back-End/Meta/06-APIs/LittleLemon/LittleLemonAPI/views.py
@@ -3,12 +3,20 @@ from rest_framework.decorators import api_view
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
-@api_view()
+
+@api_view(['GET','POST'])
 def menu_items(request):
-    items = MenuItem.objects.all()
-    serialized_items = MenuItemSerializer(items, many = True)
-    return Response(serialized_items.data)
+    if request.method == 'GET':
+        items = MenuItem.objects.select_related('category').all()
+        serialized_items = MenuItemSerializer(items, many = True)
+        return Response(serialized_items.data)
+    if request.method == 'POST':
+        serialized_items = MenuItemSerializer(data=request.data)
+        serialized_items.is_valid(raise_exception=True)
+        serialized_items.save()
+        return Response(serialized_items, status.HTTP_201_CREATED)
 
 @api_view()
 def single_item(request, id):
