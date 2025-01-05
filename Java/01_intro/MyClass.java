b9f5ee37f3cafb6ec1ec85486675a5859f03fc21
Bardia Hassanzadeh <bardia.hszd@gmail.com>
2025-01-05 12:59:54 -0800

    Initial Commit

---
+++ Java/01_intro/MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass{
+    public static void main(String args[]){
+        // print strings
+        System.out.println("Hello, World!");
+        System.out.print("\n Hello, \n World!");
+        // print numbers
+        System.out.print("\n \n 8/4="+(8/4));
+    }
+}
